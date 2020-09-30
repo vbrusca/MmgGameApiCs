3a88d25c678826f8d216deb4cda771bca68680eb
Victor G. Brusca <brusca@middlemind.net>
2020-09-30 19:07:16 -0400

    Updated project with the latest changes from java to cs port, 3.

---
+++ MmgGameApiCs/net/middlemind/MmgGameApiCs/MmgBase/MmgUpdateHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace net.middlemind.MmgGameApiCs.MmgBase
+{
+    /// <summary>
+    /// Class template for handling update events.
+    /// Created by Middlemind Games 06/01/2005
+    ///
+    /// @author Victor G.Brusca
+    /// </summary>
+    public interface MmgUpdateHandler
+    {
+        /// <summary>
+        /// Handle the incoming update event.
+        /// </summary>
+        /// <param name="obj">The update event object.</param>
+        public void MmgHandleUpdate(Object obj);
+    }
+}
