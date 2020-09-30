28371c8272c8153ea041945c47b5a842e34b8121
Victor G. Brusca <brusca@middlemind.net>
2020-09-30 08:52:10 -0400

    Updated project with the latest changes from java to cs port.

--- MmgGameApiCs/net/middlemind/MmgGameApiCs/MmgBase/Mmg9Slice.cs
+++ MmgGameApiCs/net/middlemind/MmgGameApiCs/MmgBase/Mmg9Slice.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace net.middlemind.MmgGameApiCs.MmgBase
 {
@@ -174,50 +176,67 @@ namespace net.middlemind.MmgGameApiCs.MmgBase
         /// </summary>
         public virtual void DrawDest()
         {
-            bool alpha = true;
+            //bool alpha = true;
+
             MmgBmp b = GetSrc();
-            Image img = b.GetImage();
+            Texture2D img = b.GetImage();
+            GraphicsDevice gd = MmgSettings.GRAPHICS_DEVICE;
+            SpriteBatch g = new SpriteBatch(gd);
+            RenderTarget2D bg = new RenderTarget2D(gd, GetWidth(), GetHeight());
+            g.GraphicsDevice.SetRenderTarget(bg);
+
+            /*
             BufferedImage bg = GRAPHICS_CONFIG.createCompatibleImage(GetWidth(), GetHeight(), alpha ? Transparency.TRANSLUCENT : Transparency.OPAQUE);
             Graphics2D g = (Graphics2D)bg.getGraphics();
-
             if (MmgPen.ADV_RENDER_HINTS == true)
             {
                 g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
                 g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
                 g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
             }
+            */
 
             int fs = offset;
 
             //TOP
             //draw top left
-            g.drawImage(img, 0, 0, fs, fs, 0, 0, fs, fs, null);
+            //g.drawImage(img, 0, 0, fs, fs, 0, 0, fs, fs, null);
+            //img, destination rect, source rect
+            g.Draw(img, new Rectangle(0, 0, fs, fs), new Rectangle(0, 0, fs, fs), Color.Transparent);
 
             //draw scaled top center
-            g.drawImage(img, fs, 0, GetWidth() - fs, fs, fs, 0, b.GetWidth() - fs, fs, null);
+            //g.drawImage(img, fs, 0, GetWidth() - fs, fs, fs, 0, b.GetWidth() - fs, fs, null);
+            g.Draw(img, new Rectangle(fs, 0, GetWidth() - fs, fs), new Rectangle(fs, 0, b.GetWidth() - fs, fs), Color.Transparent);
 
             //draw top right
-            g.drawImage(img, GetWidth() - fs, 0, GetWidth(), fs, b.GetWidth() - fs, 0, b.GetWidth(), fs, null);
+            //g.drawImage(img, GetWidth() - fs, 0, GetWidth(), fs, b.GetWidth() - fs, 0, b.GetWidth(), fs, null);
+            g.Draw(img, new Rectangle(GetWidth() - fs, 0, GetWidth(), fs), new Rectangle(b.GetWidth() - fs, 0, b.GetWidth(), fs), Color.Transparent);
 
             //MIDDLE
             //draw middle left
-            g.drawImage(img, 0, fs, fs, GetHeight() - fs, 0, fs, fs, b.GetHeight() - fs, null);
+            //g.drawImage(img, 0, fs, fs, GetHeight() - fs, 0, fs, fs, b.GetHeight() - fs, null);
+            g.Draw(img, new Rectangle(0, fs, fs, GetHeight() - fs), new Rectangle(0, fs, fs, b.GetHeight() - fs), Color.Transparent);
 
             //draw middle center
-            g.drawImage(img, fs, fs, GetWidth() - fs, GetHeight() - fs, fs, fs, b.GetWidth() - fs, b.GetHeight() - fs, null);
+            //g.drawImage(img, fs, fs, GetWidth() - fs, GetHeight() - fs, fs, fs, b.GetWidth() - fs, b.GetHeight() - fs, null);
+            g.Draw(img, new Rectangle(fs, fs, GetWidth() - fs, GetHeight() - fs), new Rectangle(fs, fs, b.GetWidth() - fs, b.GetHeight() - fs), Color.Transparent);
 
             //draw middle right
-            g.drawImage(img, GetWidth() - fs, fs, GetWidth(), GetHeight() - fs, b.GetWidth() - fs, fs, b.GetWidth(), b.GetHeight() - fs, null);
+            //g.drawImage(img, GetWidth() - fs, fs, GetWidth(), GetHeight() - fs, b.GetWidth() - fs, fs, b.GetWidth(), b.GetHeight() - fs, null);
+            g.Draw(img, new Rectangle(GetWidth() - fs, fs, GetWidth(), GetHeight() - fs), new Rectangle(b.GetWidth() - fs, fs, b.GetWidth(), b.GetHeight() - fs), Color.Transparent);
 
             //BOTTOM
             //draw bottom left
-            g.drawImage(img, 0, GetHeight() - fs, fs, GetHeight(), 0, b.GetHeight() - fs, fs, b.GetHeight(), null);
+            //g.drawImage(img, 0, GetHeight() - fs, fs, GetHeight(), 0, b.GetHeight() - fs, fs, b.GetHeight(), null);
+            g.Draw(img, new Rectangle(0, GetHeight() - fs, fs, GetHeight()), new Rectangle(0, b.GetHeight() - fs, fs, b.GetHeight()), Color.Transparent);
 
             //draw scaled bottom center
-            g.drawImage(img, fs, GetHeight() - fs, GetWidth() - fs, GetHeight(), fs, b.GetHeight() - fs, b.GetWidth() - fs, b.GetHeight(), null);
+            //g.drawImage(img, fs, GetHeight() - fs, GetWidth() - fs, GetHeight(), fs, b.GetHeight() - fs, b.GetWidth() - fs, b.GetHeight(), null);
+            g.Draw(img, new Rectangle(fs, GetHeight() - fs, GetWidth() - fs, GetHeight()), new Rectangle(fs, b.GetHeight() - fs, b.GetWidth() - fs, b.GetHeight()), Color.Transparent);
 
             //draw bottom right
-            g.drawImage(img, GetWidth() - fs, GetHeight() - fs, GetWidth(), GetHeight(), b.GetWidth() - fs, b.GetHeight() - fs, b.GetWidth(), b.GetHeight(), null);
+            //g.drawImage(img, GetWidth() - fs, GetHeight() - fs, GetWidth(), GetHeight(), b.GetWidth() - fs, b.GetHeight() - fs, b.GetWidth(), b.GetHeight(), null);
+            g.Draw(img, new Rectangle(GetWidth() - fs, GetHeight() - fs, GetWidth(), GetHeight()), new Rectangle(b.GetWidth() - fs, b.GetHeight() - fs, b.GetWidth(), b.GetHeight()), Color.Transparent);
 
             dest = new MmgBmp(bg);
         }
