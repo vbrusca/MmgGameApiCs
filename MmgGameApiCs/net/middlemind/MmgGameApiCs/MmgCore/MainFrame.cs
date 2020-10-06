def3e6b53cc67de402ca778372df3bdab5d65ebe
Victor G. Brusca <brusca@middlemind.net>
2020-10-06 07:34:41 -0400

    Updated project with the latest changes from the Java to CS port,2.

--- MmgGameApiCs/net/middlemind/MmgGameApiCs/MmgCore/MainFrame.cs
+++ MmgGameApiCs/net/middlemind/MmgGameApiCs/MmgCore/MainFrame.cs
@@ -3,73 +3,66 @@ using net.middlemind.MmgGameApiCs.MmgBase;
 
 namespace net.middlemind.MmgGameApiCs.MmgCore
 {
-    /**
-     * The main frame of the game, extends the JFrame class. 
-     * Handles housing the GamePanel, JPanel, that draws each game state by making the corresponding game screen the active screen. 
-     * Created by Middlemind Games 08/01/2015
-     *
-     * @author Victor G. Brusca
-     */
-    public class MainFrame : JFrame
+    /// <summary>
+    /// The main frame of the game, extends the JFrame class. 
+    /// Handles housing the GamePanel, JPanel, that draws each game state by making the corresponding game screen the active screen.
+    /// Created by Middlemind Games 08/01/2015
+    ///
+    /// @author Victor G.Brusca
+    /// </summary>
+    public class MainFrame
     {
-
-        /**
-         * The GamePanel, extends JFrame, class that handles drawing the different game states.
-         */
+        /// <summary>
+        /// The GamePanel, extends JFrame, class that handles drawing the different game states.
+        /// </summary>
         public GamePanel pnlGame;
 
-        /**
-         * The window width.
-         */
+        /// <summary>
+        /// The window width.
+        /// </summary>
         public readonly int winWidth;
 
-        /**
-         * The window height.
-         */
+        /// <summary>
+        /// The window height.
+        /// </summary>
         public readonly int winHeight;
 
-        /**
-         * The X offset of this frame.
-         */
+        /// <summary>
+        /// The X offset of this frame.
+        /// </summary>
         public readonly int myX;
 
-        /**
-         * The Y offset of this frame.
-         */
+        /// <summary>
+        /// The Y offset of this frame. 
+        /// </summary>
         public readonly int myY;
 
-        /**
-        * The game panel width.
-        */
+        /// <summary>
+        /// The game panel width. 
+        /// </summary>
         public readonly int panelWidth;
 
-        /**
-        * The game panel height.
-        */
+        /// <summary>
+        /// The game panel height. 
+        /// </summary>
         public readonly int panelHeight;
 
-        /**
-        * The game panel width.
-        */
+        /// <summary>
+        /// The game panel width.
+        /// </summary>
         public readonly int gameWidth;
 
-        /**
-        * The game panel height.
-        */
+        /// <summary>
+        /// The game panel height.
+        /// </summary>
         public readonly int gameHeight;
 
-        /*
-         * Constructor that sets the window width and height, and defaults the X, Y offsets to 0. 
-         * It also sets the JFrame and game width and height to that of the window width and height.
-         *
-         * @param WinWidth      The desired window width.
-         * @param WinHeight     The desired window height.
-         */
         /// <summary>
-        /// 
+        /// Constructor that sets the window width and height, and defaults the X, Y offsets to 0. 
+        /// It also sets the JFrame and game width and height to that of the window width and height.
         /// </summary>
-        /// <param name="WinWidth"></param>
-        /// <param name="WinHeight"></param>
+        /// <param name="WinWidth">The desired window width.</param>
+        /// <param name="WinHeight">The desired window height.</param>
         public MainFrame(int WinWidth, int WinHeight)
         {
             winWidth = WinWidth;
@@ -82,25 +75,15 @@ namespace net.middlemind.MmgGameApiCs.MmgCore
             myY = 0;
         }
 
-        /*
-         * Constructor that sets the window width and height, the JFrame width and height, and the game width and height.
-         * 
-         * @param WinWidth      The desired window width.
-         * @param WinHeight     The desired window height.
-         * @param PanWidth      The desired JFrame width.
-         * @param PanHeight     The desired JFrame height.
-         * @param GameWidth     The desired game width.
-         * @param GameHeight    The desired game height.
-         */
         /// <summary>
-        /// 
+        /// Constructor that sets the window width and height, the JFrame width and height, and the game width and height.
         /// </summary>
-        /// <param name="WinWidth"></param>
-        /// <param name="WinHeight"></param>
-        /// <param name="PanWidth"></param>
-        /// <param name="PanHeight"></param>
-        /// <param name="GameWidth"></param>
-        /// <param name="GameHeight"></param>
+        /// <param name="WinWidth">The desired window width.</param>
+        /// <param name="WinHeight">The desired window height.</param>
+        /// <param name="PanWidth">The desired JFrame width.</param>
+        /// <param name="PanHeight">The desired JFrame height.</param>
+        /// <param name="GameWidth">The desired game width.</param>
+        /// <param name="GameHeight">The desired game height.</param>
         public MainFrame(int WinWidth, int WinHeight, int PanWidth, int PanHeight, int GameWidth, int GameHeight)
         {
             MmgHelper.wr("MainFrame: WinWidth: " + WinWidth);
@@ -119,17 +102,11 @@ namespace net.middlemind.MmgGameApiCs.MmgCore
             myY = (winHeight - panelHeight) / 2;
         }
 
-        /*
-         * Sets the display text of the frame rate label.
-         *
-         * @param fr        A long representing the current drawing frame rate, or the frame rate if no time lock is applied.
-         * @param rfr       A long representing the locked frame rate.
-         */
         /// <summary>
-        /// 
+        /// Sets the display text of the frame rate label.
         /// </summary>
-        /// <param name="fr"></param>
-        /// <param name="rfr"></param>
+        /// <param name="fr">A long representing the current drawing frame rate, or the frame rate if no time lock is applied.</param>
+        /// <param name="rfr">A long representing the locked frame rate.</param>
         public virtual void SetFrameRate(long fr, long rfr)
         {
             if (pnlGame != null)
@@ -138,151 +115,98 @@ namespace net.middlemind.MmgGameApiCs.MmgCore
             }
         }
 
-        /*
-         * Sets the GamePanel for this class to use as the game being displayed.
-         * 
-         * @param gp        The GamePanel to display.
-         */
         /// <summary>
-        /// 
+        /// Sets the GamePanel for this class to use as the game being displayed.
         /// </summary>
-        /// <param name="gp"></param>
+        /// <param name="gp">The GamePanel to display.</param>
         public virtual void SetGamePanel(GamePanel gp)
         {
             pnlGame = gp;
         }
 
-        /*
-         * Gets the current GamePanel this classing is using to display a game.
-         * 
-         * @return      The current GamePanel being displayed. 
-         */
         /// <summary>
-        /// 
+        /// Gets the current GamePanel this classing is using to display a game.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>The current GamePanel being displayed.</returns>
         public virtual GamePanel GetGamePanel()
         {
             return pnlGame;
         }
 
-        /*
-         * Gets the current window width of the application.
-         * 
-         * @return      The current window width.
-         */
         /// <summary>
-        /// 
+        /// Gets the current window width of the application.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>The current window width.</returns>
         public virtual int GetWindowWidth()
         {
             return winWidth;
         }
 
-        /*
-         * Gets the current window height of the application.
-         * 
-         * @return The current window height. 
-         */
         /// <summary>
-        /// 
+        /// Gets the current window height of the application.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>The current window height.</returns>
         public virtual int GetWindowHeight()
         {
             return winHeight;
         }
 
-        /*
-         * Gets the X offset of the GamePanel in this JFrame.
-         * 
-         * @return      The X offset of the GamePanel's rendering location in the JFrame.
-         */
         /// <summary>
-        /// 
+        /// Gets the X offset of the GamePanel in this JFrame.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>The X offset of the GamePanel's rendering location in the JFrame.</returns>
         public virtual int GetOffsetX()
         {
             return myX;
         }
 
-        /*
-         * Gets the Y offset of the GamePanel in this JFrame.
-         * 
-         * @return      The Y offset of the GamePanel's rendering location in the JFrame.
-         */
         /// <summary>
-        /// 
+        /// Gets the Y offset of the GamePanel in this JFrame.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>The Y offset of the GamePanel's rendering location in the JFrame.</returns>
         public virtual int GetOffsetY()
         {
             return myY;
         }
 
-        /*
-         * Gets the width of the GamePanel in this JFrame.
-         * 
-         * @return      The width of the GamePanel in this JFrame.
-         */
         /// <summary>
-        /// 
+        /// Gets the width of the GamePanel in this JFrame.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>The width of the GamePanel in this JFrame.</returns>
         public virtual int GetGamePanelWidth()
         {
             return panelWidth;
         }
 
-        /*
-         * Gets the height of the GamePanel in this JFrame.
-         * 
-         * @return      The height of the GamePanel in this JFrame.
-         */
         /// <summary>
-        /// 
+        /// Gets the height of the GamePanel in this JFrame.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>The height of the GamePanel in this JFrame.</returns>
         public virtual int GetGamePanelHeight()
         {
             return panelHeight;
         }
 
-        /*
-         * Gets the width of the game that's rendered by the GamePanel in this JFrame.
-         * 
-         * @return      The width of the game that's rendered by the GamePanel in this JFrame.
-         */
         /// <summary>
-        /// 
+        /// Gets the width of the game that's rendered by the GamePanel in this JFrame.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>The width of the game that's rendered by the GamePanel in this JFrame.</returns>
         public virtual int GetGameWidth()
         {
             return gameWidth;
         }
 
-        /*
-         * Gets the height of the game that's rendered by the GamePanel in this JFrame.
-         * 
-         * @return      The height of the game that's rendered by the GamePanel in this JFrame.
-         */
         /// <summary>
-        /// 
+        /// Gets the height of the game that's rendered by the GamePanel in this JFrame.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>The height of the game that's rendered by the GamePanel in this JFrame.</returns>
         public virtual int GetGameHeight()
         {
             return gameHeight;
         }
 
-        /*
-         * Initializes the components used by this JFrame.
-         */
         /// <summary>
-        /// 
+        /// Initializes the components used by this JFrame.
         /// </summary>
         public virtual void InitComponents()
         {
@@ -352,11 +276,8 @@ namespace net.middlemind.MmgGameApiCs.MmgCore
             */
         }
 
-        /*
-         * Forces the GamePanel to repaint itself.
-         */
         /// <summary>
-        /// 
+        /// Forces the GamePanel to repaint itself.
         /// </summary>
         public virtual void Redraw()
         {
